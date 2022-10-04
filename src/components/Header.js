c74ced4eabb8b40c42bbde4925641e10bde43259
Gucci Fan <gucc@umich.edu>
2022-10-04 18:30:28 -0400

    commit tree c81c95f7a73061b1fb246fc15c0f045ebb2544a9

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({isDarkMode, onDarkModeClick}){  
+
+    return(
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={(e) => onDarkModeClick()}> {isDarkMode?"Dark ":"Light "}Mode
+        </button>
+      </header>
+)}
+
+export default Header;
